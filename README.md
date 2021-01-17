0dfea717b19e88d9cdeed390eb471261ccb886dd
cqrlosArch <diaz.flores.c@gmail.com>
2021-01-17 20:26:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">My Gallery</h1>
+
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://{your-demo-link.your-domain}">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://{your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/gcbWLxG6wdennelX7b8I">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [Contact](#contact)
+- [Acknowledgements](#acknowledgements)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+<p align="center">
+  <img src="screenshot.png">
+</p>
+
+
+### Built With
+
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
+
+- [Pug](https://pugjs.org/api/getting-started.html)
+- [Sass](https://sass-lang.com/)
+- [Gulp](https://gulpjs.com/)
+
+## Features
+
+<!-- List the features of your application or follow the template. Don't share the figma file here :) -->
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/gcbWLxG6wdennelX7b8I) was to build an application to complete the given user stories.
+
+
+## Acknowledgements
+
+<!-- This section should list any articles or add-ons/plugins that helps you to complete the project. This is optional but it will help you in the future. For exmpale -->
+
+- [Steps to replicate a design with only HTML and CSS](https://devchallenges-blogs.web.app/how-to-replicate-design/)
+- [Node.js](https://nodejs.org/)
+- [Marked - a markdown parser](https://github.com/chjj/marked)
+
+## Contact
+
+- Discord [CqrlosArch#0296](https://discord.com/)
+- Twitch [@cqrlosarch](https://www.twitch.tv/cqrlosarch/about)
+- GitHub [@cqrlosArch](https://github.com/cqrlosArch)
+- Twitter [@cqrlosArch](https://twitter.com/cqrlosArch)
+- Youtube [Cqrlos Arch](https://www.youtube.com/channel/UCV8eaXiCFXUN5Rkpc42G3ZQ)
---
+++ gulpfile.babel.js
@@ -0,0 +1,154 @@
+import gulp from 'gulp';
+const sourcemaps = require('gulp-sourcemaps');
+
+import { init as server, stream, reload } from 'browser-sync';
+
+const gulpEsbuild_build = require('gulp-esbuild');
+const { createGulpEsbuild } = require('gulp-esbuild');
+const gulpEsbuild_dev = createGulpEsbuild();
+
+import plumber from 'gulp-plumber';
+
+import cacheBust from 'gulp-cache-bust';
+
+import pug from 'gulp-pug';
+
+import sass from 'gulp-sass';
+import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+
+import purgecss from 'gulp-purgecss';
+
+import imagemin from 'gulp-imagemin';
+
+const mode = require('gulp-mode')();
+
+const production = mode.production() && 'true';
+
+const pluginsPostcss = [autoprefixer, cssnano];
+
+gulp.task('esbuild', async () => {
+  return gulp
+    .src('./src/js/index.js')
+    .pipe(mode.development(sourcemaps.init()))
+    .pipe(
+      mode.production(
+        gulpEsbuild_build({
+          outfile: 'bundle.js',
+          bundle: true,
+          minify: true,
+          target: 'es2015',
+        })
+      )
+    )
+    .pipe(
+      mode.development(
+        gulpEsbuild_dev({
+          outfile: 'bundle.js',
+          sourcemap:true,
+          bundle: true,
+        })
+      )
+    )
+    .pipe(mode.development(sourcemaps.write('.')))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('views', () => {
+  return gulp
+    .src('./src/views/pages/*.pug')
+    .pipe(plumber())
+    .pipe(
+      pug({
+        pretty: production ? false : true,
+      })
+    )
+    .pipe(
+      cacheBust({
+        type: 'timestamp',
+      })
+    )
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('sass', async () => {
+  return gulp
+    .src('./src/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(mode.development(sourcemaps.init()))
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      }).on('error', sass.logError)
+    )
+    .pipe(mode.production(postcss(pluginsPostcss)))
+    .pipe(mode.development(sourcemaps.write('.')))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(stream());
+});
+
+gulp.task('imgMin', () => {
+  return gulp
+    .src('./src/assets/images/**/*')
+    .pipe(plumber())
+    .pipe(
+      mode.production(
+        imagemin([
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.mozjpeg({ quality: 75, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
+          imagemin.svgo({
+            plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+          }),
+        ])
+      )
+    )
+    .pipe(gulp.dest('./public/assets/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp
+    .src('./src/assets/fonts/**/*')
+    .pipe(plumber())
+    .pipe(gulp.dest('./public/assets/fonts'));
+});
+
+gulp.task('clean-css', () => {
+  return gulp
+    .src('./public/css/*.css')
+    .pipe(plumber())
+    .pipe(
+      purgecss({
+        content: ['./public/*.html'],
+      })
+    )
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', () => {
+  server({
+    server: './public',
+  });
+  gulp.watch('./src/views/**/*.pug', gulp.series('views')).on('change', reload);
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+  //gulp.watch('./src/js/**/*.js', gulp.series('esbuild')).on('change', reload);
+  gulp.watch(
+    './src/assets/images/**/*.(svg|jpg|ico|png|jpeg)',
+    gulp.series('imgMin')
+  );
+  gulp.watch(
+    './src/assets/fonts/**/*.(ttf|eot|woff2|woff)',
+    gulp.series('fonts')
+  );
+});
+
+gulp.task(
+  'build',
+  gulp.series(
+    gulp.parallel('views', 'sass'),
+    'clean-css',
+    'imgMin',
+    'fonts'
+  )
+);
